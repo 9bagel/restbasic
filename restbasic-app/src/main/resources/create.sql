26d904fa352a3a53c6c80c0edd44d424d2f82f0c
Dzianis Bahlei <dzianis_bahlei@epam.com>
2020-06-23 14:33:45 +0300

    Added SQL for creating search function

--- restbasic-app/src/main/resources/create.sql
+++ restbasic-app/src/main/resources/create.sql
@@ -15,3 +15,9 @@ CREATE TABLE IF NOT EXISTS certificate_tags(
 	id bigserial PRIMARY KEY,
 	tag_id bigint REFERENCES tags (id),
 	certificate_id bigint REFERENCES certificates (id) ON DELETE CASCADE);
+
+CREATE OR REPLACE FUNCTION search(IN word character varying) RETURNS certificates AS $$
+SELECT * FROM certificates
+WHERE UPPER(description) LIKE UPPER('%'||word||'%')
+	OR UPPER(name) LIKE UPPER('%'||word||'%')
+$$ LANGUAGE SQL;
