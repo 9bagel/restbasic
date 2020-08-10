a42336f40f73465055e6326a1167505b521358d7
Dzianis Bahlei <dzianis_bahlei@epam.com>
2020-08-10 20:45:31 +0300

    Added tests for AuthenticationService

--- restbasic-app/src/test/java/com/epam/esm/bahlei/restbasic/service/authentication/AuthenticationServiceImplTest.java
+++ restbasic-app/src/test/java/com/epam/esm/bahlei/restbasic/service/authentication/AuthenticationServiceImplTest.java
@@ -1,20 +1,44 @@
 package com.epam.esm.bahlei.restbasic.service.authentication;
 
-import com.epam.esm.bahlei.restbasic.model.GiftCertificate;
+import com.epam.esm.bahlei.restbasic.model.User;
+import com.epam.esm.bahlei.restbasic.security.jwt.JwtTokenService;
+import com.epam.esm.bahlei.restbasic.service.UserService;
+import com.epam.esm.bahlei.restbasic.service.impl.AuthenticationService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 import java.util.Optional;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class AuthenticationServiceImplTest {
+  @Mock private JwtTokenService jwtTokenService;
+  @Mock private UserService userService;
+  @Mock private AuthenticationManager authenticationManager;
+  @InjectMocks private AuthenticationService authenticationService;
 
-    @Test
-    void getToken_NonExistingId_EmptyOptional() {
+  @Test
+  void getToken_NonExistingUser_Error() {
+    when(userService.getByUsername("username")).thenReturn(Optional.empty());
 
-    }
+    assertThrows(
+        UsernameNotFoundException.class,
+        () -> authenticationService.getToken("username", "password"));
+  }
+
+  @Test
+  void getToken_ExistingUser_OK() {
+    when(userService.getByUsername("username")).thenReturn(Optional.of(new User()));
+
+    assertThatCode(() -> authenticationService.getToken("username", "password"))
+        .doesNotThrowAnyException();
+  }
 }
