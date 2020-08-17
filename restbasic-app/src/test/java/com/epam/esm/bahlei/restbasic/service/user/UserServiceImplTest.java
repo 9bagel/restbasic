e72b8005dae49ceaa261817c92191f1064af56b1
Dzianis Bahlei <dzianis_bahlei@epam.com>
2020-08-17 14:29:00 +0300

    Test section has been moved to parent build.gradle

--- restbasic-app/src/test/java/com/epam/esm/bahlei/restbasic/service/user/UserServiceImplTest.java
+++ restbasic-app/src/test/java/com/epam/esm/bahlei/restbasic/service/user/UserServiceImplTest.java
@@ -16,6 +16,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import java.io.FileNotFoundException;
 import java.util.List;
 import java.util.Optional;
 
